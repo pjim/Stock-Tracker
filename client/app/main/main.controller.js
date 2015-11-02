ceee2cf1297ee1d676b6dc6d8e0eb6b1d1a7c96d
pjim <paul.k.james@googlemail.com>
2015-11-02 16:47:44 +0000

    place the button outside form to prevent reload

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,10 +4,8 @@ angular.module('stockTrackerApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
 
 
-        var stockCallParams = '{"Normalized":false,"NumberOfDays":300,"DataPeriod":"Day","Elements":[{"Symbol":"GOOG","Type":"price","Params":["c"]}]}'
 
-        var stockData = {};
-
+        var stockCallParams = '{"Normalized":false,"NumberOfDays":300,"DataPeriod":"Day","Elements":[{"Symbol":"GOOG","Type":"price","Params":["c"]}]}';
 
 
         $scope.chartConfig = {
@@ -22,24 +20,34 @@ angular.module('stockTrackerApp')
             },
 
             loading: false
-        } ;
+        };
 
-                var stockCall = $http.post('/stockcall',{stockOb:stockCallParams}).success(function(data){
-                  stockData = data;
-                   var dataOb = angular.fromJson(data);
-                    var stockName = dataOb.Elements[0].Symbol;
-                    var stockPrices = dataOb.Elements[0].DataSeries.close.values;
-                    console.log(stockPrices);
-                  $scope.chartConfig.series.push({name:stockName,data:stockPrices});
+        //
+        // var stockCall = $http.post('/stockcall',{stockOb:stockCallParams}).success(function(data){
+        //            var dataOb = angular.fromJson(data);
+        //             var stockName = dataOb.Elements[0].Symbol;
+        //             var stockPrices = dataOb.Elements[0].DataSeries.close.values;
+        //             console.log(stockPrices);
+        //           $scope.chartConfig.series.push({name:stockName,data:stockPrices});
+        //
+        //       });
 
-              });
+
+
+
+
+
+       $scope.trackStock = function(){
+         console.log('track activated') 
+          var stockCall = $http.post('/stockcall',{stockOb:stockCallParams}).success(function(data){
+            var dataOb = angular.fromJson(data);
+             var stockName = dataOb.Elements[0].Symbol;
+             var stockPrices = dataOb.Elements[0].DataSeries.close.values;
+             console.log(stockPrices);
+            $scope.chartConfig.series.push({name:stockName,data:stockPrices});
+
+       });
+
+      }
+
   });
-
-
-
-//note the successful example below uses quotes
-
-var anotherExampleCall = {"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"AAPL","Type":"price","Params":["c"]}]}
-
-
-var newExample = {"Normalized":false,"NumberOfDays":300,"DataPeriod":"Day","Elements":[{"Symbol":"GOOG","Type":"price","Params":["c"]}]} //note without c returned nothing of use
