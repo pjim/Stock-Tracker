3df23590e8c01fc76889c91efcfd27ff7b8a82cc
pjim <paul.k.james@googlemail.com>
2015-11-03 11:23:07 +0000

    Reset all stocks with button

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -28,8 +28,6 @@ angular.module('stockTrackerApp')
         console.log('removing' + element);
         console.log(charts)
         $scope.chartConfig.series = $scope.chartConfig.series.filter(function(ele){
-              console.log(ele.name);
-              console.log(element)
 
              if(ele.name === element){return false}
              else{return true}
@@ -41,24 +39,14 @@ angular.module('stockTrackerApp')
         $scope.buttons.splice(butLoc,1);
       }
 
-      //error it always removes the last entry in chart config array
 
-       //make an array search that can find a stock to be deleted
+      //error you cannot reinsert a removed stock without a page refresh
 
+     $scope.resetTracking = function(){
+       $scope.buttons = [];
+       $scope.chartConfig.series = [];
+     }
 
-       //implement an removal button - places the stock into the view with a removal buttons
-       //removal button uses the name of the stock to use the array search
-       //cull the offending item
-
-        //
-        // var stockCall = $http.post('/stockcall',{stockOb:stockCallParams}).success(function(data){
-        //            var dataOb = angular.fromJson(data);
-        //             var stockName = dataOb.Elements[0].Symbol;
-        //             var stockPrices = dataOb.Elements[0].DataSeries.close.values;
-        //             console.log(stockPrices);
-        //           $scope.chartConfig.series.push({name:stockName,data:stockPrices});
-        //
-        //       });
 
 
      var numberStocksTracked = 0;
